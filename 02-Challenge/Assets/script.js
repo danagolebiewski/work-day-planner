e9ba8da9023b43e0dcff0e9bff6fd55afc8074a5
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-07-22 15:46:14 -0600

    added local storage for time slots

--- 02-Challenge/Assets/script.js
+++ 02-Challenge/Assets/script.js
@@ -1,6 +1,27 @@
+// Using moment.js to display current day and time 
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("MMM Do, YYYY h:mm:ss a"));
 
+$("saveBtn").on("click", function () {
+console.log(this);
+var text = $(this).siblings("desripction").val();
+var time = $(this).parent().attr("id");
+
+// adding items to local storage
+localStorage.setItem(time,text); 
+})
+
+// load saved data from local storage 
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 // -----GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // -----THEN the current day is displayed at the top of the calendar
