1e0223fc3bd2b9fbdf28d722333ca9239cdcaa5e
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-07-22 17:01:03 -0600

    added if/else statements to change the color of time blocks

--- 02-Challenge/Assets/script.js
+++ 02-Challenge/Assets/script.js
@@ -22,17 +22,41 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
-// -----GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// -----THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
+
+function hourTracker() {
+
+  var currentHour = moment().hour(); 
+
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(blockHour, currentHour)
+  
+
+  // use these if/else statements to change the color of the hour blocks
+
+  if (blockHour < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+  } 
+  else if (blockHour === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("future");
+     $(this).removeClass("present");
+  } 
+  else {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  }
+})
+}
+// Run the function
+hourTracker();
+sessionStorage.setItem('time-block', 1);
+sessionStorage.getItem('time-block');
+
+
 // WHEN I refresh the page
 // THEN the saved events persist
 
